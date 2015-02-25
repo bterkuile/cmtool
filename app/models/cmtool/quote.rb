b0491aa997cca6e791b7352f40e2adf90191a1cd
Benjamin ter Kuile <bterkuile@gmail.com>
2015-02-25 10:11:48 +0100

    Better side menu

--- app/models/cmtool/quote.rb
+++ app/models/cmtool/quote.rb
@@ -2,27 +2,27 @@ module Cmtool
   class Quote
     include SimplyStored::Couch
     include Paperclip::Glue
-  
+
     property :owner
     property :function
     property :state
     property :body
     property :active, :type => :boolean, :default => true
     property :locale
-  
+
     property :image_file_name
     property :image_content_type
     property :image_file_size, :type => Fixnum
     property :image_updated_at, :type => Time
-  
+
     has_attached_file :image, :styles => {:medium => "300x1000>", :thumb => "137x1250>" }
-  
+
     view :for_sidebar_view, :key => [:state, :created_at], :conditions => %{doc['active']}
     STATES = %w[page sidebar]
     def self.active(*args)
       database.view(for_sidebar_view(*args))
     end
-  
+
     def self.for_page
       active(:startkey => ['page'], :endkey => ['page', {}])
     end
@@ -30,7 +30,7 @@ module Cmtool
     def self.states
       STATES
     end
-  
+
     def self.for_sidebar
       active(:startkey => ['sidebar'], :endkey => ['sidebar', {}], :limit => 2)
     end
