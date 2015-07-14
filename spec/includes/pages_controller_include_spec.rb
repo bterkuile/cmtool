d1173a94ff157528e06f558869ef73ee0b760679
Benjamin ter Kuile <bterkuile@gmail.com>
2015-07-14 14:22:10 +0200

    \fef

--- spec/includes/pages_controller_include_spec.rb
+++ spec/includes/pages_controller_include_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe PagesController, type: :controller do
   describe 'sitemap' do
-        it 'returns a proper sitemap' do
-          create_pages_tree
-          get :sitemap, format: 'xml'
-          expect( response.body ).to include "<loc>http://test.host/en/child2.2</loc>"
-        end
+    it 'returns a proper sitemap' do
+      create_pages_tree
+      get :sitemap, format: 'xml'
+      expect( response.body ).to include "<loc>http://test.host/en/child2.2</loc>"
+    end
   end
 end
