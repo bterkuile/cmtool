0bdc4b017e4e08584d1508b2871fbda3e97c5c8b
Benjamin ter Kuile <bterkuile@gmail.com>
2015-07-14 14:21:37 +0200

    Test controller page includes by means of real application controller spec

--- spec/includes/pages_controller_include_spec.rb
+++ spec/includes/pages_controller_include_spec.rb
@@ -1,15 +1,11 @@
 require 'spec_helper'
 
-class TestPagesController < ApplicationController
-  include Cmtool::Includes::PagesController
-end
-
-describe TestPagesController, type: :controller do
+describe PagesController, type: :controller do
   describe 'sitemap' do
         it 'returns a proper sitemap' do
           create_pages_tree
           get :sitemap, format: 'xml'
-          expect( response.body ).to include "<url><loc>/en/child2.2</loc></url>"
+          expect( response.body ).to include "<loc>http://test.host/en/child2.2</loc>"
         end
   end
 end
