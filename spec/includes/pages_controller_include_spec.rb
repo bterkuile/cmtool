f1e25b23a4e290946a9ed42fa527fb1f0a42a38e
Benjamin ter Kuile <bterkuile@gmail.com>
2015-07-14 14:23:41 +0200

    Only use expect syntax when it adds value

--- spec/includes/pages_controller_include_spec.rb
+++ spec/includes/pages_controller_include_spec.rb
@@ -5,7 +5,7 @@ describe PagesController, type: :controller do
     it 'returns a proper sitemap' do
       create_pages_tree
       get :sitemap, format: 'xml'
-      expect( response.body ).to include "<loc>http://test.host/en/child2.2</loc>"
+      response.body.should include "<loc>http://test.host/en/child2.2</loc>"
     end
   end
 end
